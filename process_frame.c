c6a6fa298a5845c582b6a9910074d6585c0a769b
oscar <oscar@oscar.(none)>
2013-05-15 07:47:03 +0200

    threshhold_manual

--- process_frame.c
+++ process_frame.c
@@ -1,4 +1,4 @@
-/* Copying and distribution of this file, with or without modification,
+d/* Copying and distribution of this file, with or without modification,
  * are permitted in any medium without royalty. This file is offered as-is,
  * without any warranty.
  */
